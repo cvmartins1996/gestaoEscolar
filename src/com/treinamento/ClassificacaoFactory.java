166909c2588b69b2cb561b7678f7eb78eb7b6821
cvmartins1996 <cvmartins1996@gmail.com>
2018-04-03 23:00:34 -0300

    tentativa

--- src/com/treinamento/ClassificacaoFactory.java
+++ src/com/treinamento/ClassificacaoFactory.java
@@ -5,17 +5,40 @@ public class ClassificacaoFactory  {
 	Classificacao getInstance(Aluno aluno) {
 		float media = aluno.getGrade().getMedia();
 		
-		System.out.println("media" +  media);
-		if (media == 10) {
+		return classificacaoPonderada(media);
+		
+	}
+
+	private Classificacao classificacaoPonderada(float media) {
+		
+		boolean muitoBom = media == 10;
+		boolean bom = media > 8;
+		boolean razoavel = media >  6;
+		
+		if (muitoBom) {
 			return new MuitoBom();
-		} else if (media > 8) {
+		} else if (bom) {
 			return new Bom();
-		} else if (media >  6)  {
+		} else if (razoavel)  {
 			return new Razoavel();
 		} else {
 			return new Insatisfatorio();
 		}
+		
 	}
+	
+//	public enum Tipo_Classificacao {
+//		
+//		MUITO_BOM, BOM, RAZOAVEL, INSATISFATORIO;
+//		
+//		private boolean valor;
+//		private static float media;
+//		
+//		static {
+//			MUITO_BOM.valor = media == 10;
+//			BOM.valor = media > 8 ;
+//		}
+//	}
 	
 }
 
